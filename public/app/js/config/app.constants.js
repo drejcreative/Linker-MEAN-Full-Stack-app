7255c9e18440dcb902282988ab21f5598f8875f8
Andrej Vajagic <drejcreative@gmail.com>
2017-05-26 22:20:00 +0200

    Adding toastr messages

--- public/app/js/config/app.constants.js
+++ public/app/js/config/app.constants.js
@@ -1,5 +1,6 @@
 const AppConstants = {
-  api: 'https://salty-cliffs-63626.herokuapp.com/api',
+  //api: 'https://salty-cliffs-63626.herokuapp.com/api',
+  api: 'http://localhost:3000/api',
   appName: 'Linker',
   jwtKey: 'jwtToken'
 };
